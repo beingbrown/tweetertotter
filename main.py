c0b6cdd2f86f0c171b3748b9728f0255ed15ea0c
randy brown <randy@beingbrown.net>
2019-03-27 23:19:17 -0500

    provide an interface for pruning the response attribute set
    the pruning of attributes provides a strategy for cutting down the
    attributes of a relevant response to only those attributes that are
    relevant.
    
    It may be that the stream should checkout an event, prune it, and
    check it in to a different stream topic.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import json
 import requests
 
-
-def get_stream_content(url, should_use):
+def get_stream_content(url, should_use, prune_attribs):
   r = requests.get(url, stream=True)
 
   for line in r.iter_lines():
     if line and should_use(line):
       decoded_line = line.decode('utf-8')
-      print(json.loads(decoded_line))
+      print(prune_attribs(json.loads(decoded_line)))
 
 if __name__ == '__main__':
   print "Hello World"
