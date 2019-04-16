6ccbc5356b1aad49d3a63dc390f3f332506f9c52
randy brown <randy@beingbrown.net>
2019-04-15 21:41:52 -0500

    provide a generator on the stream
    this is not ideal as it means there will very likely be a backup as
    the stream will continue to send messages that will queue--much like
    a chokepoint on a network.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_stream_content(url, should_use, prune_attribs):
   for line in r.iter_lines():
     if line and should_use(line):
       decoded_line = line.decode('utf-8')
-      print(prune_attribs(json.loads(decoded_line)))
+      yield prune_attribs(json.loads(decoded_line))
 
 if __name__ == '__main__':
   print "Hello World"
